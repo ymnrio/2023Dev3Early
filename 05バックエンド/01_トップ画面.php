a685b5703edceeb94329131f633341ff4041d248
Hinochanco <2101181@s.asojuku.ac.jp>
2023-06-12 11:41:06 +0900

    更新

--- 05バックエンド/01_トップ画面.php
+++ 05バックエンド/01_トップ画面.php
@@ -19,9 +19,6 @@
   <div class="logo">
     <img src="img/やまったーlog.png">
   </div>
-  <div class="mask">
-   <div class="circle"></div>
-  </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
   <script src="./script/script.js"></script>   
