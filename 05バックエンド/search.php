206cc5908c6e66398e96a569ed4b81650adf17b4
Hinochanco <2101181@s.asojuku.ac.jp>
2023-06-21 11:00:18 +0900

    更新

--- 05バックエンド/search.php
+++ 05バックエンド/search.php
@@ -1,6 +1,6 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;dbname=yamatter;charset=utf8','root','root');
-$sql = "SELECT * FROM post WHERE post_contents = ?";
+$sql = "SELECT * FROM post WHERE post_contents = ? OR user_name";
 $ps = $pdo->prepare($sql);
 $ps->bindValue(1,$POST['post_contents'],PDO::PARAM_STR);
 
