6007e34c3fca7fc3f0d7151d7c80a3226d51fb07
sota94 <2101212@s.asojuku.ac.jp>
2023-06-21 10:22:17 +0900

    kousin

---
+++ 05バックエンド/05_プロフィール画面.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>やまったー | プロフィール</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link href="css/nakai.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+  <link href="css/yamane.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+  <link href="css/yamanishi.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.4/css/all.css">
+</head>
+<style>
+
+</style>
+
+<body>
+  <div class="container-fluid">
+    <div class="row magin-10_ys">
+      <div class="col-md-3 col-lg-3 start_0_ys"><br>
+        <div class="log_yr">
+          <img src="img/やまったーlog.png" style="margin-left: -40px; margin-top: -75px;">
+        </div>
+        <div class="example2" style="position: fixed; margin-top: 115px;">
+          <hr class="color_yamani">
+          <input type="radio" id="1" name="example3" onclick="location.href='07_ジャンル別投稿一覧画面.html'" value="すべて"><label for="1">　♪ すべて</label>
+          <input type="radio" id="2" name="example3" onclick="location.href='07_ジャンル別投稿一覧画面.html'" value="JPOP"><label for="2">　♪ JPOP</label>
+          <input type="radio" id="3" name="example3" onclick="location.href='07_ジャンル別投稿一覧画面.html'" value="洋楽"><label for="3">　♪ 洋楽</label>
+          <input type="radio" id="4" name="example3" onclick="location.href='07_ジャンル別投稿一覧画面.html'" value="アニソン"><label for="4">　♪ アニソン</label>
+          <input type="radio" id="5" name="example3" onclick="location.href='07_ジャンル別投稿一覧画面.html'" value="クラシック"><label for="5">　♪ クラシック</label>
+          <input type="radio" id="6" name="example3" onclick="location.href='07_ジャンル別投稿一覧画面.html'" value="ロック"><label for="6">　♪ ロック</label>
+          <input type="radio" id="7" name="example3" onclick="location.href='07_ジャンル別投稿一覧画面.html'" value="VOCALOID"><label for="7">　♪ VOCALOID</label>
+          <input type="radio" id="8" name="example3" onclick="location.href='07_ジャンル別投稿一覧画面.html'" value="ギター"><label for="8">　♪ ギター</label>
+          <input type="radio" id="9" name="example3" onclick="location.href='07_ジャンル別投稿一覧画面.html'" value="楽器"><label for="9">　♪ 楽器</label>
+          <input type="radio" id="10" name="example3" onclick="location.href='07_ジャンル別投稿一覧画面.html'" value="その他"><label style="margin-bottom: -10px;" for="10">　♪ その他</label><br>
+
+          <hr class="start_0_ys color_yamani"><br>
+          <input type="radio"  id="11" name="example3" onclick="location.href='06_プロフィール編集画面.html'" value="遷移"><label class="nabi_ys" style="margin-bottom: 5px;" for="11">　プロフィール</gita-></label>
+          <hr class="start_0_ys color_yamani"><br>
+          <input type="radio" id="12" name="example3" onclick="location.href='01_トップ画面.html'" value="遷移"><label class="nabi_ys" for="12">　ログアウト</gita-></label>
+        </div>
+      </div>
+
+
+      <div class="col-md-9 col-lg-9">
+
+        <div class="row yoko_ys" style="margin-top:20px;">
+
+          <div class="col-md-2 col-lg-2"><br>
+            <img class="image_middle" src="img/pink.png">
+          </div>
+
+          <div class="col-md-8 col-lg-8" style="margin-left:-50px;"><br>
+            <p>ユーザー名
+            <p>ID:0000</p>
+
+          </div>
+
+          <div class="col-md-2 col-lg-2"><br>
+            <br><button type="button" class="btn container-fluid color_white_yamani"  onclick="location.href='06_プロフィール編集画面.html'" value="遷移"
+              style="background-color:#FBA8B8;">編集</button>
+          </div>
+
+        </div>
+
+
+
+
+        <div class="row yoko_ys">
+
+          <div class="col-md-12 start_0_ys"><br>
+            <div class="padding20_ys">
+              <h6>自己紹介文</h6><br>
+
+              <p>好きなジャンル</p>
+              <span class="border border-#FBA8B8 badge text-bg-white color_yamani">アニソン</span>
+              <span class="border border-#FBA8B8 badge text-bg-white color_yamani">JPOP</span>
+              <span class="border border-#FBA8B8 badge text-bg-white color_yamani">ギター</span>
+
+            </div>
+            <hr>
+          </div>
+        </div>
+        <div class="row yoko_ys">
+
+          <div class="wrapper">
+            <div class="ul_ys">
+              <ul class="tab">
+
+                <div class="col-md-5">
+                  <div class="middll_yamani">
+
+                    <li><a style="color:#333;text-decoration: none;" href="#toukou">投稿</a></li>
+                  </div>
+
+                </div>
+                <div class="col-md-2"></div>
+                <div class="col-md-5">
+                  <div class="middll_yamani">
+                    <li><a style="color:#333;text-decoration: none;" href="#iine">いいね</a></li>
+                  </div>
+                </div>
+
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="row yoko_ys">
+
+          <div class="col-md-12 start_0_ys back_pink_ys" style="height: 100vh;">
+
+            <hr class="start_0_ys">
+
+            <div id="toukou" class="area">
+
+              <!--ここからtweetの枠線の設定-->
+              <div class="waku_ys">
+
+                <div class="haikei_yp">
+                  <div class="padding30_ys">
+                    <div class="p_ys"><img class="image_middle" src="img/pink.png">　やまママにし<br><br>
+                      <div style="font-size: 20px;"   onclick="location.href='08_投稿詳細画面.html'" value="投稿">
+                        ギター楽しい<br>
+                        ギター楽しい<br>
+                        ギター楽しい<br><!--初心者が一番楽しい-->
+                      </div>
+                      <div class="row">
+                        <div class="col-md-9 col-lg-9 start_0_ys"></div>
+                        <div class="col-md-1 col-lg-1 start_0_ys">
+                          <input type="checkbox" id="like1">
+
+                          <label for="like1">
+                            <!--<div class="lavel_like">-->
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                              <path
+                                d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                            </svg>　18　　　
+                          </label><!--終了ラベルタグ最初はコメントの場所も指定していたけどいいねのところだけ囲った-->
+                        </div>
+                        <div class="col-md-2 col-lg-2 start_0_ys">
+                          <a href="09_投稿返信画面.html" style="text-decoration: none;">
+                            <img style="margin-left: 50px;" src="icon/コメント.svg">
+                          </a>
+                          　3　
+                        </div>
+                      </div>
+                    </div>
+                    <div class="p_ys">
+                      <img class="image_middle" src="img/pink.png">　やまママにし<br><br>
+                      <div style="font-size: 20px;" onclick="location.href='08_投稿詳細画面.html'" value="投稿">
+                        aaaa<br>
+                        <img src="img/やまったーlog.png" style="height: 200px;">
+                      </div>
+                      <div class="row">
+                        <div class="col-md-9 col-lg-9 start_0_ys"></div>
+                        <div class="col-md-1 col-lg-1 start_0_ys">
+                          <input type="checkbox" id="like2">
+
+                          <label for="like2">
+                            <!--<div class="lavel_like">-->
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                              <path
+                                d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                            </svg>　18　　　
+                          </label><!--終了ラベルタグ最初はコメントの場所も指定していたけどいいねのところだけ囲った-->
+                        </div>
+                        <div class="col-md-2 col-lg-2 start_0_ys">
+                          <a href="09_投稿返信画面.html" style="text-decoration: none;">
+                            <img style="margin-left: 50px;" src="icon/コメント.svg">
+                          </a>
+                          　3　
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--↑投稿-->
+            </div>
+
+            <div id="iine" class="area">
+              <div class="waku_ys">
+
+                <div class="haikei_yp">
+                  <div class="padding30_ys">
+                    <div class="p_ys"><img class="image_middle" src="img/pink.png"> やまママにし<br><br>
+                      <div style="font-size:20px;"  onclick="location.href='08_投稿詳細画面.html'" value="投稿">
+                        楽しい<br>
+                        楽しい<br>
+                        楽しい<br>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-9 col-lg-9 start_0_ys"></div>
+                        <div class="col-md-1 col-lg-1 start_0_ys">
+                          <input type="checkbox" id="like3">
+  
+                          <label for="like3">
+                            <!--<div class="lavel_like">-->
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                              <path
+                                d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                            </svg>　18　　　
+                          </label><!--終了ラベルタグ最初はコメントの場所も指定していたけどいいねのところだけ囲った-->
+                        </div>
+                        <div class="col-md-2 col-lg-2 start_0_ys">
+                          <a href="09_投稿返信画面.html" style="text-decoration: none;">
+                            <img style="margin-left: 50px;" src="icon/コメント.svg">
+                          </a>
+                          　3　
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="p_ys"><img class="image_middle" src="img/pink.png"> やまママにし<br><br>
+                      <div style="font-size:20px;"  onclick="location.href='08_投稿詳細画面.html'" value="投稿">
+                        楽しい<br>
+                        楽しい<br>
+                        a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>
+                        <img src="img/やまったーlog.png" style="height: 200px;">
+                      </div>
+                      <div class="row">
+                        <div class="col-md-9 col-lg-9 start_0_ys"></div>
+                        <div class="col-md-1 col-lg-1 start_0_ys">
+                          <input type="checkbox" id="like4">
+  
+                          <label for="like4">
+                            <!--<div class="lavel_like">-->
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                              <path
+                                d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                            </svg>　18　　　
+                          </label><!--終了ラベルタグ最初はコメントの場所も指定していたけどいいねのところだけ囲った-->
+                        </div>
+                        <div class="col-md-2 col-lg-2 start_0_ys">
+                          <a href="09_投稿返信画面.html" style="text-decoration: none;">
+                            <img style="margin-left: 50px;" src="icon/コメント.svg">
+                          </a>
+                          　3　
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--↑いいね-->
+            </div>
+
+            <!--↓ボタン-->
+            <div class="box">
+              <button type="button" class="btn container-fluid color_white_yamani border border-dark"
+                style=" width: 65px;height: 65px;background: #FBA8B8;border-radius: 50%;"
+                onclick="location.href='10_新規投稿作成画面.html'">＋</button>
+            </div>
+
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.4/css/all.css">
+
+            <div id="page_top"><a href="#"></a></div>
+            <!--wrapper-->
+
+          </div>
+          <div class="col-md-12 start_0_ys"></div>
+        </div>
+      </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+      integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="sprict/yamanishi.js"></script>
+    <script src="sprict/yamanisi_climb.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+      crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ 05バックエンド/06_プロフィール編集画面.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>やまったー | プロフィール編集画面</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link href="css/nakai.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+  <!--    <link href="css/yamane.css" rel="stylesheet" type="text/css"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">   -->
+  <link href="css/yamanishi.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+  <style>
+    input[type="file"] {
+      display: none;
+    }
+  </style>
+</head>
+
+<body>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3 back_pink_yamani" style="height:100vh"></div>
+
+      <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-5"></div>
+          <div class="col-md-1">
+            <img class="image_middle" src="img/pink.png" style="margin-top:25px; ">
+
+            <label class="btn container-fluid color_white_yamani aikn_ys start_0_ys border border-dark">
+              <input type="file" accept="image/*">
+              <p class="p_pusu_ys">＋</p>
+            </label>
+
+          </div>
+          <div class="col-md-6 usiro_ys">
+            <p style="margin-left:30px;margin-top: 45px;">そタコ</p>
+          </div>
+
+        </div>
+
+        <textarea class="sayu_ys form-control alert-light magin40_yamanisi "
+          style="width: 90%;height: 30px;text-align:left;border:none;overflow-wrap: break-word;margin-top: 70px;"
+          id="txt1" maxlength="200" placeholder="自己紹介"></textarea>
+        <hr class="sayu_ys">
+        <div style="text-align: right;margin-right: 30px;">
+          <p>200文字</p>
+        </div>
+        <hr class="line" id="hr_nh">
+        <div class="text-line " style="margin-bottom: 20px;">
+          <p>好きなジャンル</p>
+        </div>
+
+        <div class="example3">
+          <input type="checkbox" id="1" name="example2"><label for="1">すべて</label>
+          <input type="checkbox" id="2" name="example2"><label for="2">JPOP</label>
+          <input type="checkbox" id="3" name="example2"><label for="3">洋楽</label>
+          <input type="checkbox" id="4" name="example2"><label for="4">アニソン</label>
+        </div>
+        <div class="example3">
+          <input type="checkbox" id="5" name="example2"><label for="5">クラシック</label>
+          <input type="checkbox" id="6" name="example2"><label for="6">ロック</label>
+          <input type="checkbox" id="7" name="example2"><label for="7">VOCALOID</label>
+          <input type="checkbox" id="8" name="example2"><label for="8">ギター</label>
+        </div>
+        <div class="example3">
+          <input type="checkbox" id="9" name="example2"><label for="9">楽器</label>
+          <input type="checkbox" id="10" name="example2"><label for="10">その他</label>
+         
+        </div>
+
+
+        <div class="btn-group" data-toggle="buttons">
+
+        </div>
+        <div class="row magin40_yamanisi">
+          <div class="col-md-6" id="btn_nh">
+            <!--<div class="col-md-3" id="btn_nh">-->
+            <button onclick="location.href='05_プロフィール画面.html'" class="btn container-fluid color_white_yamani"
+              style="background-color:#FBA8B8;width: 90%;margin-left: 30px;">キャンセル</button>
+          </div>
+          <div class="col-md-6" id="btn_nh">
+            <button onclick="location.href='05_プロフィール画面.html'" class="btn container-fluid color_white_yamani"
+              style="background-color:#FBA8B8;width: 90%;margin-right: 30px;">保存</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3" style="background-color:#FBA8B8;"></div>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="sprict/yamanishi.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="sprict/yamanishi.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
