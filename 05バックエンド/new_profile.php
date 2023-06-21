e1dc02987a2536a169004dd193630d05c9827248
sota94 <2101212@s.asojuku.ac.jp>
2023-06-21 11:25:20 +0900

    こうしん

--- 05バックエンド/new_profile.php
+++ 05バックエンド/new_profile.php
@@ -15,17 +15,7 @@ $ps->bindValue(3,$_SESSION['user_id'],PDO::PARAM_STR);
 $ps->execute();
 
 
-//好きなジャンルを登録
-/*if(isset($_POST['example2'])){
-    $interests = $_POST['example2'];
-} else {
-    $interests = array(); // チェックボックスが選択されていない場合は空の配列を使用する
-}
-
-
-foreach ($interests as $interest) {
-    echo $interest;
-}*/
+//好きなギャンる
 $genre_name;
 if(isset($_POST['like_genre'])){
     foreach($_POST['like_genre'] as $row){
@@ -40,13 +30,13 @@ if(isset($_POST['like_genre'])){
             $genre_name = $roww['genre_name'];
         }
 
-        $sql="INSERT INTO favorite genre(user_id,genre_id,genre_name)VALUES(?,?,?)";
+        $sql="INSERT INTO favorite_genre(user_id,genre_id,genre_name)VALUES(?,?,?)";
         $ps=$pdo->prepare($sql);
         $ps->bindValue(1,$_SESSION['user_id'],PDO::PARAM_STR);
         $ps->bindValue(2,$_POST['like_genre'],PDO::PARAM_STR);
         $ps->bindValue(3,$genre_name,PDO::PARAM_STR);
         $ps->execute();
 
-    }//データベース名を変えたい...
+    }
 }
 ?>
