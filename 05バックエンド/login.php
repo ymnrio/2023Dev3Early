7c44977ddda45de1058190c56cb4286994b0cebc
sota94 <2101212@s.asojuku.ac.jp>
2023-06-27 04:56:10 +0900

    更新

--- 05バックエンド/login.php
+++ 05バックエンド/login.php
@@ -2,13 +2,18 @@
 session_start();
 
 $pdo = new PDO('mysql:host=localhost;dbname=yamatter;charset=utf8', 'root', 'root');
-$sql = "SELECT*FROM user WHERE email_address=?";
+$sql = "SELECT* ,count(*) FROM user WHERE email_address=?";
 $ps = $pdo->prepare($sql);
 $ps->bindValue(1, $_POST['email'], PDO::PARAM_STR);
 $ps->execute();
 $searchArray = $ps->fetchAll();
 
 foreach ($searchArray as $row) {
+    if($row['count(*)'] == 0){
+        header('Location:02_ログイン画面.php');
+        $_SESSION['error'] = "メールアドレスまたはパスワードが一致しません。";
+        break;
+    }
     if (password_verify($_POST['password'], $row['password'])) {
         $_SESSION['user'] = ['id' => $row['user_id'], 'name' => $row['user_name'], 'mail' => $row['email_address'], 'password' => $row['password'],
                                           'iconmedia' => $row['media'], 'introduction' => $row['self_introduction']];
