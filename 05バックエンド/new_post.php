c11c9fb17476d3fe5dc4394392ee168b81931aab
sota94 <2101212@s.asojuku.ac.jp>
2023-06-28 09:38:19 +0900

    更新

--- 05バックエンド/new_post.php
+++ 05バックエンド/new_post.php
@@ -13,8 +13,8 @@ if(isset($_FILES['file']['name']) && is_array($_FILES['file']['name'])) {
 }
 
 $pdo = new PDO('mysql:host=localhost;dbname=yamatter;charset=utf8','root','root');
-$sql ="INSERT into post(user_id,genre_id,post_contents,date_time,fabulous,comments,media1,media2)
-        value(?,?,?,?,?,?,?,?)";
+$sql ="INSERT into post(user_id,genre_id,post_contents,date_time,fabulous,comments,media1)
+        value(?,?,?,?,?,?,?)";
 $ps = $pdo->prepare($sql);
 
 $ps->bindValue(1,$_SESSION['user']['id'],PDO::PARAM_STR);//ユーザーID
@@ -23,11 +23,10 @@ $ps->bindValue(3,$_POST['text'],PDO::PARAM_STR);//投稿内容
 $ps->bindValue(4,$date,PDO::PARAM_STR);//日時
 $ps->bindValue(5,$zero,PDO::PARAM_STR);//いいね数
 $ps->bindValue(6,$zero,PDO::PARAM_STR);//コメント数
-$ps->bindValue(7,$media[0],PDO::PARAM_STR);//メディア1
-$ps->bindValue(8,$media[1],PDO::PARAM_STR);//メディア2
+$ps->bindValue(7,$_FILES['file']['name'],PDO::PARAM_STR);//メディア1
 $ps->execute();
 
-header('01_トップ画面.php');//modorimasu
+//header('01_トップ画面.php');//modorimasu
 
 
 //動画ファイルと画像だけ選ぶことができるようにする
