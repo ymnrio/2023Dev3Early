105d2e4630c7b549a5b99c0f7a2ed8a29394bb82
Hinochanco <2101181@s.asojuku.ac.jp>
2023-07-03 00:54:13 +0900

    更新

--- 05バックエンド/01_トップ画面.php
+++ 05バックエンド/01_トップ画面.php
@@ -23,10 +23,6 @@
       </div>
     </a>
   </div>
-  <?php
-   session_start(); 
-   echo $_SESSION['user']['id'];
-  ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
   <script src="./script/script.js"></script>   
