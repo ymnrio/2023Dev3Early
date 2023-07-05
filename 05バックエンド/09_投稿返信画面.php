2623b4d690d121c4a2faf6afcf463d203041a480
ymnrio <2101213@s.asojuku.ac.jp>
2023-07-06 00:16:02 +0900

    更新

--- 05バックエンド/09_投稿返信画面.php
+++ 05バックエンド/09_投稿返信画面.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -17,8 +20,7 @@
         }
     </style>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <link href="css/nakai.css" rel="stylesheet" type="text/css">
@@ -39,24 +41,20 @@
             <div class="col-md-6 col-lg-6 start_0_ys">
                 <div class="magin-sayu-ys  magin40_yamanisi">
 
-                    <button type="button" class="btn container-fluid color_white_yamani"
-                        style=" width: 130px;height: 40px;background: #FBA8B8;"
-                        onclick="history.back()">キャンセル</button>
+                    <button type="button" class="btn container-fluid color_white_yamani" style=" width: 130px;height: 40px;background: #FBA8B8;" onclick="history.back()">キャンセル</button>
 
-                    <form method="POST" action="08_投稿詳細画面.php">
+                    <form method="POST" action="new_reply.php">
 
                         <div class="touroku_ys">
-                            <button type="submit" class="btn container-fluid color_white_yamani"
-                                style=" width: 130px;height: 40px;background: #FBA8B8;"
-                                onclick="location.href='08_投稿詳細画面.php'">返信する</button>
+                            <button type="hidden" name="newreply" value="<?php echo $_POST['reply']; ?>"
+                                    class="btn container-fluid color_white_yamani" style=" width: 130px;height: 40px;background: #FBA8B8; margin-left:-35px;">返信する</button>
                         </div>
 
                         <img class="image_middle magin20_yamanisi" src="img/pink.png">
-                        <h5 style="position: relative;top:-50px;left:100px;">そた♩</h5>
+                        <h5 style="position: relative;top:-50px;left:100px;"><?php echo $_SESSION['user']['name']; ?></h5>
 
 
-                        <textarea class="form-control alert-light toukou_bokku_ys" id="txt1" maxlength="300" required
-                            placeholder="｜返信内容"></textarea><br>
+                        <textarea name="replyctt" class="form-control alert-light toukou_bokku_ys" id="txt1" maxlength="300" required placeholder="｜返信内容"></textarea><br>
                         <!--   <a class="a1_ys">メディアを選択</a><br>-->
 
 
@@ -74,8 +72,6 @@
                                 }
                             </script>
                         </label>
-
-                    </form>
                     </form>
                 </div>
             </div>
@@ -83,9 +79,7 @@
             <div class="col-md-3 col-lg-3 back_pink_ys"></div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
