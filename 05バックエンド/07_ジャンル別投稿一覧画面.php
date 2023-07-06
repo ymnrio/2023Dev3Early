7f3c911fb47742633bec74565f51ca44f70c09e7
sota94 <2101212@s.asojuku.ac.jp>
2023-07-07 05:28:10 +0900

    kousinn

--- 05バックエンド/07_ジャンル別投稿一覧画面.php
+++ 05バックエンド/07_ジャンル別投稿一覧画面.php
@@ -105,9 +105,10 @@
                   //他人のプロフィールに遷移
 echo             '<form action="13_他人プロフィール.php" method="post">'.
                     '<button name="user_id" type="hidden" value="'.$user_id.'" style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none; text-align:right;position: relative;top: -65px;left: 775px;">
-                      <span class="material-symbols-outlined">person</span></a>
+                      <span class="material-symbols-outlined">face</span>
                     </button>
                   </form>';
+                  
 echo             '<form action="08_投稿詳細画面.php" method="post">'.
                   '<button name="detail" type="hidden" value="'.$row['post_id'].'" style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none; width: 870px; text-align:left;">'.
                   '<div style="font-size: 20px;">';
