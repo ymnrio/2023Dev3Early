152e456225bacfaf5f1468c1b4219c6b2ab8749f
sota94 <2101212@s.asojuku.ac.jp>
2023-07-07 01:26:21 +0900

    kousinn

--- 05バックエンド/05_プロフィール画面.php
+++ 05バックエンド/05_プロフィール画面.php
@@ -17,6 +17,7 @@
   <link href="css/yamanishi.css" rel="stylesheet" type="text/css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.4/css/all.css">
+  <link href=”https://use.fontawesome.com/releases/v6.0.0/css/all.css” rel=”stylesheet”>
 </head>
 <style>
 
@@ -75,7 +76,7 @@
                 echo  '<img class="image_middle" width="250"src="data:image/jpeg;base64,' .  $base64_image . '" />　';
 
               } else { //設定してない場合
-                echo '<img class="image_middle" src="img/pink.png">　';
+                echo '<img class="image_middle" src="img/pink.png">';
               };
             ?>
           </div>
@@ -269,7 +270,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                               <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                             </svg>　18　　　
-                          </label><!--終了ラベルタグ最初はコメントの場所も指定していたけどいいねのところだけ囲った-->
+                          </label>
                         </div>
                         <div class="col-md-2 col-lg-2 start_0_ys">
                           <a href="09_投稿返信画面.php" style="text-decoration: none;">
