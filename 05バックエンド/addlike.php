be346925dab162381f079b8504e85ac1995f9cf9
sota94 <2101212@s.asojuku.ac.jp>
2023-07-14 02:55:23 +0900

    kousinn

--- 05バックエンド/addlike.php
+++ 05バックエンド/addlike.php
@@ -10,7 +10,7 @@ $favorite = $dataArray[1];
 $pdo = new PDO('mysql:host=localhost;dbname=yamatter;charset=utf8','root','root');
 $a = substr($favorite,0,2);
 //追加なのか削除なのかの分岐
-if($like == 1){
+if($like == 2){
         //いいねしたのが投稿か返信か判別
         if($a == "00"){
                 //返信のカウントを追加したいいね数を取得
@@ -60,7 +60,7 @@ if($like == 1){
                 //返信のカウントを取り消したいいね数を取得
                 $sql = "SELECT fabulous FROM reply WHERE reply_id = ?";
                 $ps = $pdo->prepare($sql);
-                $ps->bindValue(1,$_POST['favorite'],PDO::PARAM_STR);
+                $ps->bindValue(1,$favorite,PDO::PARAM_STR);
                 $ps->execute();
                 foreach($ps as $row){
                         $newfabulous = $row['fabulous'] - 1;
@@ -70,14 +70,14 @@ if($like == 1){
                 $sql = "UPDATE reply SET fabulous = ? WHERE reply_id = ?";
                 $ps = $pdo->prepare($sql);
                 $ps->bindValue(1,$newfabulous,PDO::PARAM_INT);
-                $ps->bindValue(2,$_POST['favorite'],PDO::PARAM_STR);
+                $ps->bindValue(2,$favorite,PDO::PARAM_STR);
                 $ps->execute();
 
         }else{
                 //投稿のカウントを取り消したいいね数を取得
                 $sql = "SELECT fabulous FROM post WHERE post_id = ?";
                 $ps = $pdo->prepare($sql);
-                $ps->bindValue(1,$_POST['favorite'],PDO::PARAM_STR);
+                $ps->bindValue(1,$favorite,PDO::PARAM_STR);
                 $ps->execute();
                 foreach($ps as $row){
                         $newfabulous = $row['fabulous'] - 1;
@@ -87,14 +87,14 @@ if($like == 1){
                 $sql = "UPDATE post SET fabulous = ? WHERE post_id = ?";
                 $ps = $pdo->prepare($sql);
                 $ps->bindValue(1,$newfabulous,PDO::PARAM_INT);
-                $ps->bindValue(2,$_POST['favorite'],PDO::PARAM_STR);
+                $ps->bindValue(2,$favorite,PDO::PARAM_STR);
                 $ps->execute();
         }
 
                 //いいねを取り消した投稿/返信をfavorite_postから削除
                 $sql = "DELETE FROM favorite_post WHERE like_subject = ?";
                 $ps = $pdo->prepare($sql);
-                $ps->bindValue(1,$_POST['favorite'],PDO::PARAM_STR);
+                $ps->bindValue(1,$favorite,PDO::PARAM_STR);
                 $ps->execute();
 
 };
