5f13771b8405f566b6580eb590d8e5497d993d68
ymnrio <2101213@s.asojuku.ac.jp>
2023-07-14 10:15:44 +0900

    更新

--- 05バックエンド/drop.php
+++ 05バックエンド/drop.php
@@ -2,16 +2,26 @@
 session_start();
 if(!empty($_POST['drop'])){
 
-$id = $_POST['drop'];
-echo $id;
+    $id = $_POST['drop'];
+    echo $id;
 
-$pdo = new PDO('mysql:host=localhost;dbname=yamatter;charset=utf8', 'root', 'root');
-$sql = "DELETE FROM post WHERE post_id = ?";
-$ps = $pdo->prepare($sql);
-$ps->bindValue(1, $id, PDO::PARAM_STR);
-$ps->execute();
-unset($_SESSION['trash']);
-header('Location:05_プロフィール画面.php');
+    $pdo = new PDO('mysql:host=localhost;dbname=yamatter;charset=utf8', 'root', 'root');
+    $a = substr($id,0,2);
+    if($a != "00"){
+        $sql = "DELETE FROM post WHERE post_id = ?";
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1, $id, PDO::PARAM_STR);
+        $ps->execute();
+        unset($_SESSION['trash']);
+        header('Location:05_プロフィール画面.php');
+    }else{
+        $sql = "DELETE FROM reply WHERE reply_id = ?";
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1, $id, PDO::PARAM_STR);
+        $ps->execute();
+        unset($_SESSION['trash']);
+        header('Location:05_プロフィール画面.php');
+    }
 
 }else{
 
