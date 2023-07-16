0ec556f3e3a5e2b5a8be8d893a1ca327ab90ea8d
ymnrio <2101213@s.asojuku.ac.jp>
2023-07-17 02:59:25 +0900

    更新

--- 05バックエンド/08_投稿詳細画面.php
+++ 05バックエンド/08_投稿詳細画面.php
@@ -75,7 +75,7 @@ unset($_SESSION['trash']);
                     $pdo = new PDO('mysql:host=localhost;dbname=yamatter;charset=utf8', 'root', 'root');
                     $a = substr($_POST['detail'], 0, 2);
                     if ($a == "00") {
-                      $sql = "SELECT reply.reply_id, reply.user_id, reply.reply_contents, reply.date_time, reply.fabulous, reply.comments, reply.media1, reply.media2, 
+                      $sql = "SELECT reply.reply_id, reply.reply_subject, reply.user_id, reply.reply_contents, reply.date_time, reply.fabulous, reply.comments, reply.media1, reply.media2, 
                               user.user_name, user.email_address, user.password, user.media, user.self_introduction 
                               FROM reply INNER JOIN user ON reply.user_id = user.user_id 
                               WHERE reply_id = ?";
@@ -83,49 +83,135 @@ unset($_SESSION['trash']);
                       $ps->bindValue(1, $_POST['detail'], PDO::PARAM_STR);
                       $ps->execute();
                       foreach ($ps as $row) {
-                        //返信元の投稿を表示
+                      //返信元の投稿を表示
                         $sql6 = "SELECT * FROM reply WHERE reply_id = ?";
                         $ps6 = $pdo->prepare($sql6);
                         $ps6->bindValue(1, $row['reply_subject'], PDO::PARAM_STR);
                         $ps6->execute();
                         foreach ($ps6 as $row6) {
-                          echo '<p>'.$row6['reply_id'].'</p>';
+                        
+                          echo '<div class="p_ys">';
+                          if (!empty($row['media']) || isset($row['media'])) { //設定している場合
+
+                            $base64_image = base64_encode($row['media']);
+      
+                            echo '<br>' . '<img class="image_middle" width="250"src="data:image/jpeg;base64,' .  $base64_image . '" />　';
+      
+                          } else { //設定してない場合
+                            echo '<img class="image_middle" src="img/pink.png">　';
+                          }
+                          echo   $row['user_name'] ;
+                          //他人のプロフィールに遷移
+echo                      '<form action="13_他人プロフィール.php" method="post">'.
+                          '<button name="user_id" type="hidden" value="'.$row['user_id'].'" style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none; text-align:right;position: relative;top: -65px;left: 775px;">
+                          <span class="material-symbols-outlined">face</span>
+                          </button>
+                          </form>
+                          <div style="font-size: 20px;">'
+                          . $row['reply_contents'];
+                          //画像があるか検索
+                          $sql2 = "SELECT * FROM reply WHERE reply_id = ?";
+                          $ps2 = $pdo->prepare($sql2);
+                          $ps2->bindValue(1, $row['reply_id'], PDO::PARAM_INT);
+                          $ps2->execute();
+                          $row2 = $ps2->fetch(PDO::FETCH_ASSOC);
+                          if (!empty($row2['media1'])) {
+                            $image_data = $row2['media1'];
+
+                            $base64_image = base64_encode($image_data);
+
+                            echo '<br>' . '<img width="250"src="data:image/jpeg;base64,' .  $base64_image . '" /><br>';
+                          }
+                          echo '</button>
+                          <p style="margin-top:20px;color:#FBA8B8;padding-left:15px;width: 300px;">'.$row['date_time'].'</p>
+                          </form>';
+                          $pdo = new PDO('mysql:host=localhost;dbname=yamatter;charset=utf8', 'root', 'root');
+                          $sql3 = "select * from favorite_post where user_id = ? and like_subject = ?";
+                          $ps3 = $pdo->prepare($sql3);
+                          $ps3->bindValue(1,$_SESSION['user']['id'],PDO::PARAM_INT);
+                          $ps3->bindValue(2,$row['reply_id'],PDO::PARAM_STR);
+                          $ps3->execute();
+                          $check_like = null;
+                          foreach($ps3 as $row3){
+                            $check_like = $row3['like_id'];
+                          } 
+                          echo '<div style="position: relative;top:-45px;left:640px;width: 150px;height:30px;">';
+
+                          if(isset($check_like)){//いいね判別
+                            echo '<form action="addlike.php" method="post">';
+                            $like = "like".$row['reply_id'];
+                            echo '<button type="hidden" name="like" value="1,'.$row['reply_id'].'" style="width:90px;background-color:white;border:none;">
+                            <input type="checkbox" checked="checked" id="'.$like.'">
+                            <label for="'.$like.'">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path
+                              d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                            </svg>　'.$row['fabulous'].'　　　';
+echo                        '</label>
+                            </button>
+                            </form>';
+                          }else{
+                            echo '<form action="addlike.php" method="post">';
+                            $like = "like".$row['reply_id'];
+                            echo '<button type="hidden" name="like" value="2,'.$row['reply_id'].'" style="width:90px;background-color:white;border:none;">
+                            <input type="checkbox" id="'.$like.'">
+                            <label for="'.$like.'">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path
+                              d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                            </svg>　'.$row['fabulous'].'　　　';
+                            echo '</label>
+                            </button>
+                            </form>';
+                          }    
+                          echo '<form action="09_投稿返信画面.php" method="post">
+                          <button name="reply" type="hidden" value="' . $row['reply_id'] . '" style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none;">
+                          <img style="margin-left: 137px; margin-top:-75px;" src="icon/コメント.svg">
+                          </button>
+                          </form>
+                          <div style="position: relative;top:-70px;left:190px;">
+                          　' . $row['comments'].
+                          '</div>
+                          </div>
+                          </div>
+                          </div>';
                         }
+                      }/**/
                         echo '<div class="p_ys">';
                         //アイコン表示
-                  if (!empty($row['media']) || isset($row['media'])) { //設定している場合
+                      if (!empty($row['media']) || isset($row['media'])) { //設定している場合
 
-                    $base64_image = base64_encode($row['media']);
+                        $base64_image = base64_encode($row['media']);
 
-                    echo '<br>' . '<img class="image_middle" width="250"src="data:image/jpeg;base64,' .  $base64_image . '" />　';
+                        echo '<br>' . '<img class="image_middle" width="250"src="data:image/jpeg;base64,' .  $base64_image . '" />　';
 
-                  } else { //設定してない場合
-                    echo '<img class="image_middle" src="img/pink.png">　';
-                  }
+                      } else { //設定してない場合
+                        echo '<img class="image_middle" src="img/pink.png">　';
+                      }
 
-                  echo   $row['user_name'] ;
-                  //他人のプロフィールに遷移
-echo             '<form action="13_他人プロフィール.php" method="post">'.
-                    '<button name="user_id" type="hidden" value="'.$row['user_id'].'" style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none; text-align:right;position: relative;top: -65px;left: 775px;">
+                      echo   $row['user_name'] ;
+                      //他人のプロフィールに遷移
+                      echo '<form action="13_他人プロフィール.php" method="post">'.
+                      '<button name="user_id" type="hidden" value="'.$row['user_id'].'" style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none; text-align:right;position: relative;top: -65px;left: 775px;">
                       <span class="material-symbols-outlined">face</span>
-                    </button>
-                  </form>
+                      </button>
+                      </form>
                       <div style="font-size: 20px;">'
-                          . $row['reply_contents'];
-                        //画像があるか検索
-                        $sql2 = "SELECT * FROM reply WHERE reply_id = ?";
-                        $ps2 = $pdo->prepare($sql2);
-                        $ps2->bindValue(1, $row['reply_id'], PDO::PARAM_INT);
-                        $ps2->execute();
-                        $row2 = $ps2->fetch(PDO::FETCH_ASSOC);
+                      . $row['reply_contents'];
+                      //画像があるか検索
+                      $sql2 = "SELECT * FROM reply WHERE reply_id = ?";
+                      $ps2 = $pdo->prepare($sql2);
+                      $ps2->bindValue(1, $row['reply_id'], PDO::PARAM_INT);
+                      $ps2->execute();
+                      $row2 = $ps2->fetch(PDO::FETCH_ASSOC);
 
-                        if (!empty($row2['media1'])) {
-                          $image_data = $row2['media1'];
+                      if (!empty($row2['media1'])) {
+                        $image_data = $row2['media1'];
 
-                          $base64_image = base64_encode($image_data);
+                        $base64_image = base64_encode($image_data);
 
-                          echo '<br>' . '<img width="250"src="data:image/jpeg;base64,' .  $base64_image . '" /><br>';
-                        }
+                        echo '<br>' . '<img width="250"src="data:image/jpeg;base64,' .  $base64_image . '" /><br>';
+                      }
                         /*　いいね実装した時にバグが心配なのでの残してます
                         echo '</div>
                             <div class="row">
@@ -151,60 +237,59 @@ echo             '<form action="13_他人プロフィール.php" method="post">'
                                 </div>
                               </div>
                             </div>*/
-echo                   '</button>
-                         <p style="margin-top:20px;color:#FBA8B8;padding-left:15px;width: 300px;">'.$row['date_time'].'</p>
-                    </form>';
-                          $pdo = new PDO('mysql:host=localhost;dbname=yamatter;charset=utf8', 'root', 'root');
-                          $sql3 = "select * from favorite_post where user_id = ? and like_subject = ?";
-                          $ps3 = $pdo->prepare($sql3);
-                          $ps3->bindValue(1,$_SESSION['user']['id'],PDO::PARAM_INT);
-                          $ps3->bindValue(2,$row['reply_id'],PDO::PARAM_STR);
-                          $ps3->execute();
-                          $check_like = null;
-                          foreach($ps3 as $row3){
-                          $check_like = $row3['like_id'];
-                          }
-echo                      '<div style="position: relative;top:-45px;left:640px;width: 150px;height:30px;">';
+                      echo '</button>
+                      <p style="margin-top:20px;color:#FBA8B8;padding-left:15px;width: 300px;">'.$row['date_time'].'</p>
+                      </form>';
+                      $pdo = new PDO('mysql:host=localhost;dbname=yamatter;charset=utf8', 'root', 'root');
+                      $sql3 = "select * from favorite_post where user_id = ? and like_subject = ?";
+                      $ps3 = $pdo->prepare($sql3);
+                      $ps3->bindValue(1,$_SESSION['user']['id'],PDO::PARAM_INT);
+                      $ps3->bindValue(2,$row['reply_id'],PDO::PARAM_STR);
+                      $ps3->execute();
+                      $check_like = null;
+                      foreach($ps3 as $row3){
+                        $check_like = $row3['like_id'];
+                      }
+                      echo '<div style="position: relative;top:-45px;left:640px;width: 150px;height:30px;">';
 
                       if(isset($check_like)){//いいね判別
-echo                      '<form action="addlike.php" method="post">';
-                            $like = "like".$row['reply_id'];
-echo                    '<button type="hidden" name="like" value="1,'.$row['reply_id'].'" style="width:90px;background-color:white;border:none;">
-                          <input type="checkbox" checked="checked" id="'.$like.'">
-                          <label for="'.$like.'">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                              <path
-                                d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-                                </svg>　'.$row['fabulous'].'　　　';
-echo                        '</label>
-                          </button>
-                          </form>';
-                        }else{
-echo                     '<form action="addlike.php" method="post">';
-                            $like = "like".$row['reply_id'];
-echo                       '<button type="hidden" name="like" value="2,'.$row['reply_id'].'" style="width:90px;background-color:white;border:none;">
-                              <input type="checkbox" id="'.$like.'">
-                                <label for="'.$like.'">
-                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                    <path
-                                      d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-                                  </svg>　'.$row['fabulous'].'　　　';
-echo                            '</label>
-                            </button>
-                            </form>';
-                        }               
-echo                          '<form action="09_投稿返信画面.php" method="post">
-                                  <button name="reply" type="hidden" value="' . $row['reply_id'] . '" style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none;">
-                                  <img style="margin-left: 137px; margin-top:-75px;" src="icon/コメント.svg">
-                                  </button>
-                                </form>
-                            <div style="position: relative;top:-70px;left:190px;">
-                            　' . $row['comments'].
-                              '</div>
-                              </div>
-                            </div>
-                          </div>';
-                      }
+                        echo '<form action="addlike.php" method="post">';
+                        $like = "like".$row['reply_id'];
+                        echo '<button type="hidden" name="like" value="1,'.$row['reply_id'].'" style="width:90px;background-color:white;border:none;">
+                        <input type="checkbox" checked="checked" id="'.$like.'">
+                        <label for="'.$like.'">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                          d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                        </svg>　'.$row['fabulous'].'　　　';
+                        echo '</label>
+                        </button>
+                        </form>';
+                      }else{
+                        echo '<form action="addlike.php" method="post">';
+                        $like = "like".$row['reply_id'];
+                        echo '<button type="hidden" name="like" value="2,'.$row['reply_id'].'" style="width:90px;background-color:white;border:none;">
+                        <input type="checkbox" id="'.$like.'">
+                        <label for="'.$like.'">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                          d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                        </svg>　'.$row['fabulous'].'　　　';
+                        echo '</label>
+                        </button>
+                        </form>';
+                      }               
+                      echo '<form action="09_投稿返信画面.php" method="post">
+                      <button name="reply" type="hidden" value="' . $row['reply_id'] . '" style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none;">
+                      <img style="margin-left: 137px; margin-top:-75px;" src="icon/コメント.svg">
+                      </button>
+                      </form>
+                      <div style="position: relative;top:-70px;left:190px;">
+                      　' . $row['comments'].
+                      '</div>
+                      </div>
+                      </div>
+                      </div>';
                     } else {
                       $sql = "SELECT post.post_id, post.user_id, post.genre_id, post.post_contents, post.date_time, post.fabulous, post.comments, post.media1, post.media2, 
                               user.user_name, user.email_address, user.password, user.media, user.self_introduction 
@@ -319,7 +404,7 @@ echo                          '<form action="09_投稿返信画面.php" method="
                 <!--⇩返信-->
                 <div class="hensin_waku_ys">
                   <?php
-                  $sql = "SELECT reply.reply_id, reply.user_id, reply.reply_contents, reply.date_time, reply.fabulous, reply.comments, reply.media1, reply.media2, 
+                  $sql = "SELECT reply.reply_id, reply.reply_subject, reply.user_id, reply.reply_contents, reply.date_time, reply.fabulous, reply.comments, reply.media1, reply.media2, 
                               user.user_name, user.email_address, user.password, user.media, user.self_introduction 
                               FROM reply INNER JOIN user ON reply.user_id = user.user_id 
                               WHERE reply_subject = ? ORDER BY date_time DESC";
